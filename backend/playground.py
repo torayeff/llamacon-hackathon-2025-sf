82a0e02b9682c7988a36980ca19bb9aff174d77f
Agajan Torayev <torayeff@gmail.com>
2025-05-03 12:18:43 -0700

    image understanding test

--- backend/playground.py
+++ backend/playground.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from dotenv import load_dotenv
@@ -5,13 +6,35 @@ from openai import OpenAI
 
 load_dotenv()
 
+model = "Llama-4-Maverick-17B-128E-Instruct-FP8"
+
 client = OpenAI(
     api_key=os.environ.get("LLAMA_API_KEY"), base_url="https://api.llama.com/compat/v1/"
 )
 
-completion = client.chat.completions.create(
-    model="Llama-4-Maverick-17B-128E-Instruct-FP8",
-    messages=[{"role": "user", "content": "Which planet do humans live on?"}],
+prompt = "Wha colour is the floor to the right of the image?"
+with open("../localdata/example.png", "rb") as image_file:
+    b64_image = base64.b64encode(image_file.read()).decode("utf-8")
+
+response = client.chat.completions.create(
+    model=model,
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/png;base64,{b64_image}",
+                    },
+                },
+            ],
+        }
+    ],
 )
 
-print(completion.choices[0].message.content)
+print(response.choices[0])
